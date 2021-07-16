7a46c12c5b7a993e26341d607bd61518fbd438d7
Tamir Yurovskiy <tamir.yurovskiy@intel.com>
2021-07-16 20:12:36 +0300

    addProduct contorller

---
+++ VeganStore/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeganStore.Controllers
+{
+    class ProductController
+    {
+        public static bool AddProduct()
+        { }
+
+        public static bool 
+    }
+}
